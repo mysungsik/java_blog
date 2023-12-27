bd78d5e6312db15d80e96ab2c049cb934cc29b62
DESKTOP-5JGR7R0\Amgine <mschoi@amgine.co.kr>
2023-12-27 12:06:09 +0900

    테스트 커밋

--- src/main/java/com/cos/blog/test/BlogControllerTest.java
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 // 스프링이 com.cos.blog 패키지 이하의 패키지들을 스캔하여
 // 특정 어노테이션이 붙어있는 클래스 파일들을 new 하여 스프링 컨테이너에서 관리하게한다.
+
 @RestController	
 public class BlogControllerTest {
-	
 	@GetMapping("/test/hello")
 	public String hello() {
 		return "<h1>hello Spring Boot !!!!! </h1>";
